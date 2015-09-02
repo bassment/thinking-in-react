38a4ef72c9b9afca73926fef85cd4edfe753df1a
admin <admin@AMCP9.local>
2015-09-02 14:12:35 +0300

    Basic webpack configuration with vannila js hello world

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+    {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: path.resolve(ROOT_PATH, 'app')
+    }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'React Tutorial'
+    })
+  ]
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+  });
+}
